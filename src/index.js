a295585b003386dff44b8c3ad983627a7623358b
Egor <egor.marmilev96@gmail.com>
2024-08-09 09:54:50 +0300

    feat: task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const pairs = Object.fromEntries(bracketsConfig);
+
+  for (const char of str) {
+    if (pairs[char]) {
+      if (stack.length === 0 || stack.pop() !== pairs[char]) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
 }
