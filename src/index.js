beb527ae23858a4abfebf435fdba3c02bebd021b
Egor <egor.marmilev96@gmail.com>
2024-08-09 11:55:25 +0300

    d

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function check(str, bracketsConfig) {
   const stack = [];
   const brackets = Object.fromEntries(bracketsConfig.map(([key, value]) => [value, key]));
 
-  for (const currect of brackets) {
+  for (const currect of str) {
     if (currect in brackets) {
       if (brackets[currect] !== stack.pop()) return false;
     } else {
